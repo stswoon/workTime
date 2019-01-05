6af7b04d6179a5c6959198177ec7661c13b27f30
stswoon <stswoon@yandex.ru>
2019-01-05 17:57:07 +0400

    time

--- components/Separator.js
+++ components/Separator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {StyleSheet, View} from "react-native";
+import Dash from 'react-native-dash';
 
 const styles = StyleSheet.create({
     container: {
@@ -10,7 +11,9 @@ const styles = StyleSheet.create({
 export default class Separator extends React.Component {
     render() {
         return (
-            <View style={styles.container}/>
+            <View style={styles.container}>
+                <Dash style={{width:100, height:1}}/>
+            </View>
         )
     }
 }
